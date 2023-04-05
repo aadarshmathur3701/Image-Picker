9d19cd5ffe3ea98da9696578a93349e28ebb1f6b
aadarshm <aadarsh.mathur@octalinfosolution.com>
2023-04-05 15:37:12 +0530

    Photo picker added

--- app/src/main/java/com/am/imagePicker/MainActivity.kt
+++ app/src/main/java/com/am/imagePicker/MainActivity.kt
@@ -2,10 +2,33 @@ package com.am.imagePicker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.am.imagePicker.databinding.ActivityMainBinding
+import com.am.imagePicker.picker.ImagePicker
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val imagePicker = ImagePicker.with(this){ uri ->
+        if (uri != null) {
+            binding.selectedImage.setImageURI(uri)
+        } else {
+            Toast.makeText(
+                this,
+                "Something went wrong!",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ActivityMainBinding.inflate(layoutInflater).also {
+            binding = it
+            setContentView(binding.root)
+        }
+        binding.btnPickPhoto.setOnClickListener {
+            imagePicker.getImageFromCamera(this)
+        }
     }
 }
